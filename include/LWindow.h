7154fad232ced840a83fa4327df23a75ff7acffc
mshres24 <mshres24@gmail.com>
2022-07-16 11:23:09 -0600

    added some credit links

--- include/LWindow.h
+++ include/LWindow.h
@@ -3,6 +3,7 @@
 
 /*This source code copyrighted by Lazy Foo' Productions (2004-2022)
 and may not be redistributed without written permission.*/
+//https://lazyfoo.net/tutorials/SDL/36_multiple_windows/index.php
 
 //Using SDL, standard IO, strings, and string streams
 #include <SDL2/SDL.h>
--- main.cpp
+++ main.cpp
@@ -398,7 +398,7 @@ class Simulation {
                 //Handle events on queue
                 while( SDL_PollEvent( &e ) != 0 )
                 {
-                cout<<"HERE";
+                //cout<<"HERE";
                     //User requests quit
                     if( e.type == SDL_QUIT )
                     {
